2efb2e4cc317af063031cd731a42df60b483319f
Carol Faria <carolineborges87@gmail.com>
2025-03-03 16:22:01 -0300

    alteracoes aula 3

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,1 +1,19 @@
-export { };
+import { Navigate, Route, Routes as Switch } from "react-router-dom";
+import { BrowserRouter, } from "react-router-dom";
+
+import { Dashboard } from "../pages";
+
+export const Routes = () => {
+
+    return (
+        <BrowserRouter>
+
+            <Switch>
+        
+                <Route path="/pagina-inicial" element={<Dashboard/> } />
+
+                <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
