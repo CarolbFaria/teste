9a353c888a685dc2cdbf81358f9fbbd51c19625c
Carol Faria <carolineborges87@gmail.com>
2025-03-03 19:37:32 -0300

    aulas 08 a 10

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,17 @@
-import { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+   const inputPasswordRef = useRef<HTMLInputElement>(null);
+   
     const [password, setPassword] = useState ('');
     const [email, setEmail] = useState ('');
-    
+
+    const emailength = useMemo(() => {
+     console.log('Executou')
+     return email.length * 1000;   
+    }, [email.length]);
+
 useEffect(() => {
     if (window.confirm('Voce é homem?')) {
         console.log('Homem')
@@ -20,23 +28,32 @@ useEffect(() => {
     console.log(email) 
 }, [email]);
 
-    const handleEntrar = () => {
+    const handleEntrar = useCallback (() => {
         console.log(email)
         console.log(password)
-    }
+    }, [email, password]);
 
     return (
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {email.length}</p>
                
                 <label>
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                    <input value={email} 
+                    onChange={e => setEmail(e.target.value)}
+                    onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
                
                 <label>
                     <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input
+                     type="password" 
+                     value={password} 
+                     ref={inputPasswordRef}
+                     onChange={e => setPassword(e.target.value)} 
+                     />
                 </label>
             
             <button type="button" onClick={handleEntrar}>
